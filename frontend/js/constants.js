7c0196e853de34a2d1b2004c1ca1e87e9b78d7e0
Jeff <jeffprymowicz@gmail.com>
2022-06-13 09:59:27 +0200

    style update

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0x902193292583A593157aA9f80cF85002ae1B2Bc9"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the Magic Maidens NFT Project!!";
+const welcome_h1 = "Welcome to the Magic Maidens NFT Project!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'Magic Maidens is a collection of 1000 NFTs, created by Jeff Prymowicz, that live on the Polygon Blockchain. Mint yours now to wield the power of a Magic Maiden!';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
